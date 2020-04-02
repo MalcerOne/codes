4f4be558286f00e89be0b9f9b7929c6c0e26dea1
Rafael Malcervelli <48256253+MalcerOne@users.noreply.github.com>
2020-04-02 12:50:20 -0300

    atualização js index/categoria

--- assets/js/categoria.js
+++ assets/js/categoria.js
@@ -2,21 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
   let params = coDesExtract()
   let value = params['key']
   console.log(value)
-  let value2 = document.querySelector(".book-link")
-  value2.href = 'livro.html?pi=' + value + '&key={{@key}}'
+  let value2 = document.querySelector(".link")
+  value2.href = 'categoria.html?pi=' + value + '&key={{@key}}'
+  let value3 = document.querySelector(".title")
+  value3.href = 'projeto.html?pi=' + value + '&key={{@key}}'
 
   let db = coDesConnect('https://projeto-ana-andre-leo-rafael.firebaseio.com/')
 
   db.download('/', function(data) {
-
-    context = data
-	console.log(context)
-	coDesReplace('.nav', context)
+      context = data
+    	console.log(context) 
+    	coDesReplace('.nav', context)
     
-    context = data['biblioteca'][value]
-    console.log(context)
-    coDesReplace('.call',context)
-    coDesReplace('.book-list',context)
+      context = data['portfolio'][value]
+      console.log(context)
+      coDesReplace('.centralizadortitulo',context)
+      coDesReplace('.title',context)
 
   })
 })
